227ba6638b2cbcf1cdc8015677ba2888c15b33fc
Rocky Daniel <48907996+Rockydaniel@users.noreply.github.com>
2023-07-14 14:53:41 +0700

    Update install-docker.sh
    update sudo -E (file ownership)

--- install-docker.sh
+++ install-docker.sh
@@ -4,7 +4,7 @@
 sudo apt-get -y update
 
 #Install packages to allow apt to use a repository over HTTPS
-sudo apt-get -y install \
+sudo -E apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,25 +12,25 @@ sudo apt-get -y install \
     lsb-release
     
 #Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo -E gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 #set up the stable repository
 echo \
 "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo -E tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 
 #Update the apt package index
 sudo apt-get -y update
 
 #Install Docker Engine and containerd
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo -E apt-get -y install docker-ce docker-ce-cli containerd.io
 
 #If occur "sudo: add-apt-repository: command not found", run below command and then try install again
-sudo apt-get -y install software-properties-common
+sudo -E apt-get -y install software-properties-common
 
 #Enable Docker
 sudo systemctl enable docker --now
 
 #Install Docker Compose
-sudo apt-get -y install docker-compose
+sudo -E apt-get -y install docker-compose
